24b2a3b4cd1b05af107f785b44654a4be602057f
hunterMotko <hunterMotko@gmail.com>
2022-05-09 12:35:58 -0400

    re-setup auth and adding to frontend

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,18 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './NavBar.jsx';
 import Footer from './Footer.jsx';
 import Home from './routes/Home.jsx';
 import Profile from './routes/Profile.jsx';
+import Login from './routes/Login.jsx';
 
 const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
   return (
     <Router>
       <NavBar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/profile' element={<Profile />} />
+        <Route path='/login' element={<Login setIsAuthenticated={setIsAuthenticated}/>} />
       </Routes>
       <Footer />
     </Router>
---
+++ server/controllers/users.js
@@ -0,0 +1,54 @@
+
+const { checkUserAlreadyExists, createUser, getUserPassword } = require('../models/users.js');
+const { newToken, checkPassword } = require('../utils/auth');
+
+const signUp = async(req, res) => {
+  const { email, password, first_name, last_name, address } = req.body;
+
+  try {
+    const user = await checkUserAlreadyExists(email);
+
+    if (user.rows.length > 0) {
+      return res.status(401).json({ msg: 'User already exists' });
+    }
+
+    const newUser = await createUser(email, password, first_name, last_name, address);
+    console.log(newUser);
+    const token = newToken(newUser.rows[0].id);
+
+    return res.json({token});
+  } catch (err) {
+    console.error('SIGN_UP', err);
+    res.sendStatus(400);
+  }
+};
+
+const signIn = async(req, res) => {
+  const {email, password} = req.body;
+
+  try {
+    const user = await getUserPassword(email);
+
+    if (user.rows.length === 0) {
+      return res.sendStatus(401).json({ msg: 'Unauthorized' });
+    }
+
+    const check = await checkPassword(password, user.rows[0].password);
+
+    if (!check) {
+      return res.sendStatus(401).json({ msg: 'Unauthorized' });
+    }
+
+    const token = newToken(user.rows[0].id);
+
+    return res.json({token});
+  } catch (err) {
+    console.log('SIGN_IN', err);
+    res.sendStatus(400);
+  }
+};
+
+module.exports = {
+  signUp,
+  signIn
+};
