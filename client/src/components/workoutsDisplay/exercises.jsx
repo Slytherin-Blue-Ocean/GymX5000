9cd965a6d05990deff73bd4301df1337d98d6c22
Benjamin Cope <brc1379@gmail.com>
2022-05-10 14:49:27 -0700

    commit before merge

--- client/src/components/workoutsDisplay/exercises.jsx
+++ client/src/components/workoutsDisplay/exercises.jsx
@@ -5,27 +5,30 @@ import ActivityModal from '../subcomponents/ActivityModal.jsx';
 
 const Exercise = () => {
   const [exercise, setExercise] = useState('');
-  const [selected, setSelected] = useState(false);
-
-  const workoutId = 1;
 
   useEffect(() => {
-    axios.get(`http://localhost:3001/workout/${workoutId}`)
+    axios.get('http://localhost:3001/workout/1')
       .then((response) => {
         console.log(response.data);
-        // set some stats to this data
+        const result = response.data[0];
+        setExercise({
+          category: result.bodyCategory,
+          equipment: result.equipment,
+          gif_url: result.gif_url.slice(0, result.gif_url.length - 1),
+          exercise_name: result.exercise_name,
+        });
       })
       .catch((err) => console.log(err));
-  }, [exercise]);
+  }, []);
 
   return (
     <div>
       <div>Temp Activity Card</div>
-      <h2>Name</h2>
-      <div>Target</div>
-      <div>Required Equipment</div>
+      <h2>{exercise.exercise_name}</h2>
+      <div>{exercise.category}</div>
+      <div>{exercise.equipment}</div>
 
-      <img src="http://d205bpvrqc9yn1.cloudfront.net/0001.gif" alt="this is a gif, hopefully" />
+      <img src={exercise.gif_url} alt={exercise.gif_url} />
       {/* <ActivityModal /> */}
     </div>
   );
