d6f53fecb173db5009a096c91db665d6c0e6d5b0
es98dame <dmkr.kim@gmail.com>
2022-05-10 16:38:41 -0700

    tags completed

--- server/models/model.js
+++ server/models/model.js
@@ -3,15 +3,21 @@ const { pool } = require('../utils/db');
 module.exports = {
   getAllActivities: (limit, page, callback) => {
     pool.query(`(
-      SELECT A.id As activity_id, A.name As type, exercise.id AS reference_id, exercise.exercise_name AS name, exercise.gif_url AS thumbnail_url, A.tags FROM exercise
+      SELECT A.id, A.name As type, exercise.id As activity_id, exercise.exercise_name AS activity,
+	exercise.gif_url AS thumbnail_url, ARRAY[exercise.body_category, exercise.equipment, exercise.target_muscle] as tags
+	FROM exercise
       INNER JOIN activitytype as A ON A.id = exercise.activitytype_id
-      AND A.id = exercise.activitytype_id OFFSET ${page - 1} limit ${limit / 2}
+      AND A.id = exercise.activitytype_id
+	group by A.id, exercise.id OFFSET ${page - 1} limit ${limit / 2}
       )
       UNION
       (
-      SELECT C.id As activity_id, C.name As type, A.ID AS reference_id, A.name AS name, A.image AS thumbnail_url, C.tags FROM food as A
-      INNER JOIN activitytype as C ON C.id = A.activitytype_id
-      AND C.id = A.activitytype_id OFFSET ${page - 1} limit ${limit / 2}
+        SELECT C.id, C.name As type, A.id  As activity_id, A.name AS activity, A.image AS thumbnail_url,
+        ARRAY[A.dietlabel, A.healthlabel] as tags
+        FROM food as A
+        INNER JOIN activitytype as C ON C.id = A.activitytype_id
+        AND C.id = A.activitytype_id
+        group by C.id, A.id OFFSET ${page - 1} limit ${limit / 2}
       );
     `)
       .then((res) => callback(null, res))
