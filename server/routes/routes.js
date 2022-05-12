97fade24d90db9d777288e20f4e8fdcc548dc024
Ryan De Leon <ryandeleon0@gmail.com>
2022-05-12 11:45:25 -0700

    started testing

---
+++ server/routes/routes.js
@@ -0,0 +1,41 @@
+const { Router } = require('express');
+const router = Router();
+const { getUser } = require('../controllers/users');
+const activity = require('../controllers/activity');
+const exercise = require('../controllers/exercise');
+console.log(activity);
+router.get('/user', getUser);
+
+router.get('/activities', activity.getAllActivities);
+
+router.get('/recipes', activity.getallrecipes);
+
+router.get('/recipes/:foodId', activity.getrecipes);
+
+router.get('/competition', activity.getcompetitions);
+
+router.get('/quotes', activity.getquotes);
+
+router.get('/favorite', activity.getfavor);
+
+router.post('/favorite', activity.postfavor);
+
+router.delete('/favorites/:id', activity.deleteFavor);
+
+router.get('/classes', activity.getallclass);
+
+router.post('/bookclass', activity.bookclass);
+
+router.post('/cancelclass', activity.cancelclass);
+
+router.get('/classhistory', activity.getclasshistory);
+
+router.get('/favoriteclass', activity.getfavoriteclass);
+
+router.get('/workout', exercise.getallworkout);
+
+router.get('/workout/:workoutId', exercise.getworkout);
+
+module.exports = {
+  router
+};
