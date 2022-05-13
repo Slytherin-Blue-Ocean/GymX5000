2f19db626d83da42cb51d3d2712dd302f9c1112f
hunterMotko <hunterMotko@gmail.com>
2022-05-13 13:36:44 -0400

    home page complete

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Navbar, NavDropdown, Container, FormControl, Form, Nav, Button } from 'react-bootstrap';
+import { Navbar, NavDropdown, Container, FormControl, Form, Nav} from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import {useAuth} from './context/Auth.jsx';
+import {Stack, Button} from '@mui/material';
 
 const NavBar = () => {
   const { logout, isAuth } = useAuth();
@@ -9,19 +10,32 @@ const NavBar = () => {
   return (
     <Navbar bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand><Link className="nav-link" to="/activity">GymX5000</Link></Navbar.Brand>
+        <Navbar.Brand><Link className="nav-link text-danger h3 p-0" to="/">GymX5000</Link></Navbar.Brand>
         {
-          isAuth &&
-          <>
-            <Nav className="me-auto">
-              <Link className="nav-link" to="/activity">Activities</Link>
-              <Link className="nav-link" to="/classes">Class</Link>
-              <Link className="nav-link" to="/challenges">Challenges</Link>
-              <Link className="nav-link" to="/singlechallenge">Placeholder</Link>
-              <Link className="nav-link" to="/profile">My Profile</Link>
-            </Nav>
-            <Button onClick={logout}>Log out</Button>
-          </>
+          isAuth ?
+            <>
+              <Nav className="me-auto">
+                <Link className="nav-link" to="/activity">Activities</Link>
+                <Link className="nav-link" to="/classes">Class</Link>
+                <Link className="nav-link" to="/challenges">Challenges</Link>
+                <Link className="nav-link" to="/singlechallenge">Placeholder</Link>
+                <Link className="nav-link" to="/profile">My Profile</Link>
+              </Nav>
+              <Button onClick={logout} variant="outlined" size="large" color="error" >Log out</Button>
+            </>
+            :
+            <>
+              <div className="me-auto">
+              </div>
+              <Stack direction="row" spacing={2}>
+                <Button variant="outlined" color="error" href="/login">
+                  Login
+                </Button>
+                <Button variant="outlined" color="error" href="/register">
+                  Register
+                </Button>
+              </Stack>
+            </>
         }
       </Container>
     </Navbar>
--- client/src/components/routes/Home.jsx
+++ client/src/components/routes/Home.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import arnold from '../images/arnold.webp';
+import food from '../images/foodpic.jpeg';
+import exercise from '../images/exercise-gif.jpeg';
+import classEx from '../images/class.jpeg';
+import Kettlebell from '../threejs/Kettlebell.js';
+
 import {
   Container,
   Box,
@@ -11,6 +16,7 @@ import {
   Paper
 } from '@mui/material';
 import { styled } from '@mui/material/styles';
+import {useAuth} from '../context/Auth.jsx';
 
 const Item = styled(Paper)(() => ({
   backgroundImage: `url(${arnold})`,
@@ -23,43 +29,74 @@ const Item = styled(Paper)(() => ({
 }));
 
 const Item2 = styled(Paper)(() => ({
-  margin: '1em',
+  background: 'rgba(30,33,36,0.8)',
+  margin: '0 auto',
   height: '100%',
   width: '100%',
-  color: '#ccc'
+  color: '#FFF'
 }));
 
 const Home = () => {
+  const { isAuth } = useAuth();
+
   return (
     <>
       <Grid container>
         <Grid item style={{width: '100vw', height: '80vh'}}>
           <Item elevation={24}>
             <br />
-            <h1 style={{marginLeft: '2rem'}}>Welcome To GYMX5000</h1>
+            <h1 style={{marginLeft: '3rem', fontSize: '60px'}}>Welcome To GYMX5000</h1>
           </Item>
         </Grid>
       </Grid>
       <Grid container direction={'row'} justifyContent="center" spacing={5}>
-        <Grid item style={{background: '#CCC', margin: '1em', padding: '2rem', borderRadius: '5px'}}>
+        <Grid item style={{background: '#CCC', margin: '1em', padding: '2rem', borderRadius: '5px', zIndex: 1}}>
           <h2>Classes</h2>
+          <img src={classEx} alt="class exercises" height="250" width="250" style={{borderRadius: '5px'}}/>
         </Grid>
-        <Grid item style={{background: '#CCC', margin: '1em', padding: '2rem', borderRadius: '5px'}}>
+        <Grid item style={{background: '#CCC', margin: '1em', padding: '2rem', borderRadius: '5px', zIndex: 1}}>
           <h2>Exercises</h2>
+          <img src={exercise} alt="exercise" height="250" width="200" style={{borderRadius: '5px'}}/>
         </Grid>
-        <Grid item style={{background: '#CCC', margin: '1em', padding: '2rem', borderRadius: '5px'}}>
+        <Grid item style={{background: '#CCC', margin: '1em', padding: '2rem', borderRadius: '5px', zIndex: 1}}>
           <h2>Nutrition</h2>
+          <img src={food} alt="Stew Picture" height="250" width="250" style={{borderRadius: '5px'}}/>
         </Grid>
-        <Grid item style={{background: '#CCC', margin: '1em', padding: '2rem', borderRadius: '5px'}}>
+        <Grid item style={{background: '#CCC', margin: '1em', padding: '2rem', borderRadius: '5px', zIndex: 1}}>
           <h2>Challenges</h2>
+          <div className="single-badge" style={{ width: '250px', borderRadius: '5px'}}><Kettlebell /></div>
         </Grid>
       </Grid>
-
-      <Grid container>
-        <Grid item>
-          <h3>
-            Healthy Habits Made Simple
-          </h3>
+      <Grid container style={{marginTop: '-3em'}}>
+        <Grid item style={{ margin: '0 auto', width: '100vw', height: 'fit-content'}}>
+          <Item2>
+            <Grid container direction="column" justifyContent="center" alignItems="center" style={{textAlign: 'center'}}>
+              <Grid item style={{marginTop: '4em'}}>
+                <Typography variant="h2" gutterBottom>
+                  Healthy Habits Made Simple
+                </Typography>
+              </Grid>
+              <Grid item style={{margin: '0 5rem'}}>
+                <Typography variant="h3">
+                  Our Mission
+                </Typography>
+                <Typography variant="subtitle1" style={{width: '50%', margin: '0 auto'}}>Your transformation is our passion. We are your personal trainer, your nutritionist, your supplement expert, your lifting partner, your support group. We provide the technology, tools, and products you need to burn fat, build muscle, and become your best self.</Typography>
+              </Grid>
+              <Grid item style={{margin: '1rem'}}>
+                {
+                  !isAuth &&
+                  <>
+                    <Button variant="outlined" size="large" color="error" style={{margin: '1rem'}} href="/login">
+                      Login
+                    </Button>
+                    <Button variant="outlined" size="large" color="error" style={{margin: '1rem'}} href="/register">
+                      Register
+                    </Button>
+                  </>
+                }
+              </Grid>
+            </Grid>
+          </Item2>
         </Grid>
       </Grid>
     </>
