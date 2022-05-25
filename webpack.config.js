294dbf13a18a350cfb2ba2e893805ac16634fdff
Owen Yoshishige <owenyoshishige@gmail.com>
2022-05-24 20:43:54 -0700

    Fix webpack for server starting
    Fixed bug where webpack would not properly build bundle.

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,7 @@ var SRC_DIR = path.join(__dirname, '/client/src');
 var DIST_DIR = path.join(__dirname, '/client/dist');
 
 module.exports = {
-  mode: 'production',
-  minimize: true,
+  mode: 'development',
   entry: `${SRC_DIR}/index.jsx`,
   output: {
     path: DIST_DIR,
